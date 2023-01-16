44a3bfca0e8374dfb5458545dea5391e4d688d1f
CJ Gammon <cjgammon@missys-mbp-2.lan>
2023-01-15 22:33:38 -0600

    add more ui

--- src/index.ts
+++ src/index.ts
@@ -22,5 +22,5 @@ ScrollSmoother.create({
   normalizeScroll: true,
   smooth: 1, // how long (in seconds) it takes to "catch up" to the native scroll position
   effects: true, // looks for data-speed and data-lag attributes on elements
-  smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+  smoothTouch: false, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
 });
